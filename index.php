1e6099e86f61655acceefcc08a822c3eec0214b4
Henrique Neto <henriqueneto1912@gmail.com>
2021-09-18 15:42:52 -0300

    Adicionado parte superior do header (CSS)

--- index.php
+++ index.php
@@ -4,9 +4,51 @@
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <!-- Links CSS -->
+        <link rel="stylesheet" href="css/main/header.main.css">
         <title>Document</title>
     </head>
     <body>
-        Teste
+        <header>
+            <section class="headerContainer">
+                <!-- Topo do header -->
+                <div class="headerTop">
+                    <div class="headerTopContainer">
+                        <p class="info infoNum">
+                            <svg width="18" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path id="Vector" d="M23.7427 18.2015C22.8493 16.5123 19.7469 14.6808 19.6103 14.6007C19.2117 14.3737 18.7957 14.2536 18.4064 14.2536C17.8275 14.2536 17.3537 14.5188 17.0667 15.0012C16.6129 15.544 16.0501 16.1786 15.9135 16.2769C14.8564 16.9942 14.0288 16.9128 13.1132 15.997L8.00289 10.8855C7.09303 9.97556 7.00939 9.13767 7.7217 8.08618C7.82136 7.94868 8.45581 7.38533 8.99861 6.93101C9.34475 6.72498 9.58234 6.41884 9.68645 6.04327C9.82482 5.54356 9.72293 4.95574 9.39636 4.38305C9.31939 4.25134 7.48723 1.14805 5.79922 0.254974C5.48421 0.0881061 5.12917 0 4.77324 0C4.18684 0 3.63514 0.22872 3.22048 0.642996L2.09128 1.77191C0.305389 3.55762 -0.341075 5.58183 0.168799 7.78804C0.594139 9.6267 1.83368 11.5833 3.8536 13.603L10.3961 20.1465C12.9526 22.7033 15.3898 24 17.6402 24C17.6402 24 17.6402 24 17.6407 24C19.2958 24 20.8387 23.2965 22.2255 21.9095L23.3543 20.7806C24.0403 20.0949 24.1961 19.0581 23.7427 18.2015Z" fill="#055DE8"/>
+                            </svg>
+                            <span>(00) 00 12345-6789</span>
+                        </p>
+                        <p class="info infoEmail">
+                            <svg width="18" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <g id="email">
+                                    <g id="Group">
+                                        <g id="Group_2">
+                                            <path id="Vector" d="M22.8 9.8675L21.6 10.6645L12.6639 16.5996C12.4628 16.7332 12.2315 16.8 12 16.8C11.7686 16.8 11.5372 16.7332 11.3361 16.5996L2.4 10.6646L1.2 9.86759L0 9.07053V22.8C0 23.4627 0.537375 24 1.2 24H22.8C23.4628 24 24 23.4627 24 22.8V9.07043L22.8 9.8675Z" fill="#055DE8"/>
+                                        </g>
+                                    </g>
+                                    <g id="Group_3">
+                                        <g id="Group_4">
+                                            <path id="Vector_2" d="M12.6649 0.201551C12.2619 -0.0671836 11.7368 -0.0671836 11.3336 0.201551L1.17773 6.97213L2.26148 7.69189L3.34519 8.41175L11.9992 14.1595L20.6533 8.41161L21.7371 7.69185L22.8207 6.97199L12.6649 0.201551Z" fill="#055DE8"/>
+                                        </g>
+                                    </g>
+                                </g>
+                            </svg>
+                            <span>ecommerce@commerce.com</span>
+                        </p>
+                        <a href="#" class="info infoAccount" >Minha conta</a>
+                    </div>
+                </div>
+                <!-- Meio do header -->
+                <div class="headerMiddle">
+                    <div class="headerMiddleContainer"></div>
+                </div>
+                <!-- Inferior do header -->
+                <div class="headerBottom">
+                    <div class="headerBottomContainer"></div>
+                </div>
+            </section>
+        </header>
     </body>
 </html>
